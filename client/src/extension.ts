15f427b72f3117efdd1bdb17d1fec280f23d6684
xieguigang <xie.guigang@gcmodeller.org>
2021-05-25 22:16:37 +0800

    add source

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -2,64 +2,91 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
+// tslint:disable
+"use strict";
 
-import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
+import * as path from "path";
 
+import { workspace, Disposable, ExtensionContext } from "vscode";
 import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
-} from 'vscode-languageclient/node';
-
-let client: LanguageClient;
+    LanguageClient,
+    LanguageClientOptions,
+    SettingMonitor,
+    ServerOptions,
+    TransportKind,
+    InitializeParams,
+    StreamInfo,
+    createServerPipeTransport,
+} from "vscode-languageclient/node";
+import { Trace, createClientPipeTransport } from "vscode-jsonrpc/node";
+import { createConnection } from "net";
 
 export function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+    // The server is implemented in node
+    let serverExe = "dotnet";
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	let serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
-	};
+    // let serverExe = "D:\\Development\\Omnisharp\\csharp-language-server-protocol\\sample\\SampleServer\\bin\\Debug\\netcoreapp2.0\\win7-x64\\SampleServer.exe";
+    // let serverExe = "D:/Development/Omnisharp/omnisharp-roslyn/artifacts/publish/OmniSharp.Stdio.Driver/win7-x64/OmniSharp.exe";
+    // The debug options for the server
+    // let debugOptions = { execArgv: ['-lsp', '-d' };5
 
-	// Options to control the language client
-	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
-	};
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    let serverOptions: ServerOptions = {
+        // run: { command: serverExe, args: ['-lsp', '-d'] },
+        run: {
+            command: serverExe,
+            args: ["D:/Development/Omnisharp/csharp-language-server-protocol/sample/SampleServer/bin/Debug/netcoreapp3.1/win7-x64/SampleServer.dll"],
+            transport: TransportKind.pipe,
+        },
+        // debug: { command: serverExe, args: ['-lsp', '-d'] }
+        debug: {
+            command: serverExe,
+            args: ["D:/Development/Omnisharp/csharp-language-server-protocol/sample/SampleServer/bin/Debug/netcoreapp3.1/win7-x64/SampleServer.dll"],
+            transport: TransportKind.pipe,
+            runtime: "",
+        },
+    };
+    // let time = 100;
+    // let serverOptions = async () => {
+    //     await new Promise((r) => setTimeout(r, time));
+    //     time = 10000;
+    //     const [reader, writer] = createServerPipeTransport("\\\\.\\pipe\\" + "samplepipe");
+    //     return {
+    //         reader,
+    //         writer,
+    //     };
+    // };
 
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'languageServerExample',
-		'Language Server Example',
-		serverOptions,
-		clientOptions
-	);
+    // Options to control the language client
+    let clientOptions: LanguageClientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [
+            {
+                pattern: "**/*.cs",
+            },
+            {
+                pattern: "**/*.csx",
+            },
+            {
+                pattern: "**/*.cake",
+            },
+        ],
+        progressOnInitialization: true,
+        synchronize: {
+            // Synchronize the setting section 'languageServerExample' to the server
+            configurationSection: "languageServerExample",
+            fileEvents: workspace.createFileSystemWatcher("**/*.cs"),
+        },
+    };
 
-	// Start the client. This will also launch the server
-	client.start();
-}
+    // Create the language client and start the client.
+    const client = new LanguageClient("languageServerExample", "Language Server Example", serverOptions, clientOptions);
+    client.registerProposedFeatures();
+    client.trace = Trace.Verbose;
+    let disposable = client.start();
 
-export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+    // Push the disposable to the context's subscriptions so that the
+    // client can be deactivated on extension deactivation
+    context.subscriptions.push(disposable);
 }
