4635dce0d741914a60f8ad886680ea94f366c7a0
guigang xie <xie.guigang@gcmodeller.org>
2023-11-17 13:57:58 +0800

    run poackage msbuild

---
+++ @export/index.d.ts
@@ -0,0 +1,11 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=Julia
+
+declare namespace Julia {
+   module _ {
+      /**
+      */
+      function onLoad(): object;
+   }
+}
